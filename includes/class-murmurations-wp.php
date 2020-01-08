7ebcc3b078b5278fccc08143f2403e2bdd0158c4
AM <adam@photosynth.ca>
2020-01-07 19:47:39 -0800

    Removed default output of JSON-LD in HTML head

--- includes/class-murmurations-wp.php
+++ includes/class-murmurations-wp.php
@@ -94,8 +94,9 @@ class Murmurations_WP {
 
 		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+    
+    //$this->loader->add_action('wp_head',$plugin_public,'show_structured_data');
 
-    $this->loader->add_action('wp_head',$plugin_public,'show_structured_data');
     $this->loader->add_action( 'rest_api_init',$plugin_public,'register_api_route');
 
 	}
